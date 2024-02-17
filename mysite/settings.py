186f216a83fa2bce92b382e0c12f1e012b69047f
Mehvish Fatima <87938552+ivanMehvish@users.noreply.github.com>
2024-02-17 16:24:55 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2zr)a3b4((7nql2oz4f+5@oxwtmn)yrsvh7we^2e%oh0%9*ix=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
