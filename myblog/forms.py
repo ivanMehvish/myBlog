c1e26cbcefc44ab84edda49b0ab841ad0dbc73e5
ivanMehvish <hsivhem321@gmail.com>
2024-02-17 16:16:01 +0530

    first

--- myblog/forms.py
+++ myblog/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 from .models import Post
 
 class postForm(forms.ModelForm):
@@ -7,3 +8,16 @@ class postForm(forms.ModelForm):
     class Meta:
         model=Post
         fields=('title', 'text',)
+
+
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=65)
+    password = forms.CharField(max_length=15, widget=forms.PasswordInput)
+    
+
+class RegisterForm(UserCreationForm):
+    class Meta:
+        model=User
+        fields = ['username','email','password1','password2'] 
--- myblog/views.py
+++ myblog/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render,get_object_or_404,redirect
 from .models import Post
 from django.utils import timezone
 from .forms import postForm
+from django.contrib.auth.decorators import login_required 
+from django.contrib import messages
+from django.contrib.auth import login, authenticate, logout
+from .forms import LoginForm, RegisterForm
+
 def post_list(request):
     posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'myblog/post_list.html', {"posts": posts})
@@ -10,6 +15,7 @@ def post_detail(request, pk):
     post=get_object_or_404(Post, pk=pk)
     return render(request, 'myblog/post_detail.html', {'post': post})
 
+@login_required
 def post_new(request):
     if request.method == "POST":
          form = postForm(request.POST)
@@ -24,6 +30,22 @@ def post_new(request):
     return render(request, 'myblog/post_edit.html', {'form':form})
 
 
+def signup(request):
+    if request.method == 'GET':
+        form = RegisterForm()
+        return render(request, 'myblog/register.html', {'form': form})    
+   
+    if request.method == 'POST':
+        form = RegisterForm(request.POST) 
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            messages.success(request, 'You have singed up successfully.')
+            login(request, user)
+            return redirect('')
+        else:
+            return render(request, 'myblog/register.html', {'form': form})
 
 
 
